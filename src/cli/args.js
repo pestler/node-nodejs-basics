2b8adbbed455996fec383083b0537d41e140e5ae
Aleh Kuis <pestleralex@gmail.com>
2024-10-03 15:07:19 +0300

    feat: args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+    const propResult = process.argv
+        .slice(2)
+        .reduce((acc, el, i, arr) => {
+            if ((i < arr.length - 1) && (i % 2 === 0)) {
+                acc.push(
+                    `${el.slice(2)} is ${arr[i + 1]}`
+                )
+            }
+            return acc
+        }, [])
+        .join(', ')
+
+    console.log(propResult);
 };
 
 parseArgs();
