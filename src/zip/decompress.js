39101626544396a05ba9d3d323b9cfc3273c2388
Aleh Kuis <pestleralex@gmail.com>
2024-10-06 17:24:17 +0300

    feat: decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream';
+import path from 'path';
+import zlib from 'zlib';
+
+import { getPathUrl } from '../util/get-url-path.js'
+
+const filesDir = 'files';
+
 const decompress = async () => {
-    // Write your code here 
+    const sourcePath = path.resolve(getPathUrl(import.meta.url), filesDir, 'archive.gz');
+    const targetPath = path.resolve(getPathUrl(import.meta.url), filesDir, 'fileToCompress.txt');
+    const readFile = createReadStream(sourcePath);
+    const writeFile = createWriteStream(targetPath);
+
+
+    pipeline(
+        readFile,
+        zlib.createGunzip(),
+        writeFile,
+        (error) => error && process.stderr.write(error)
+        );
 };
 
 await decompress();
