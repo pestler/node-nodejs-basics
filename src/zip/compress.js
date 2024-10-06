6c164c16ef2807bb4b23d70fb92434b5ee9815cd
Aleh Kuis <pestleralex@gmail.com>
2024-10-06 17:19:16 +0300

    feat: compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,27 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream';
+import path from 'path';
+import zlib from 'zlib';
+
+import { getPathUrl } from '../util/get-url-path.js'
+
+const filesDir = 'files';
+
 const compress = async () => {
-    // Write your code here 
+
+    const sourcePath = path.resolve(getPathUrl(import.meta.url), filesDir, 'fileToCompress.txt');
+    const targetPath = path.resolve(getPathUrl(import.meta.url), filesDir, 'archive.gz');
+    const readFile = createReadStream(sourcePath);
+    const writeFile = createWriteStream(targetPath);
+
+
+    pipeline(
+        readFile,
+        zlib.createGzip(),
+        writeFile,
+        (error) => error && process.stderr.write(error)
+        );
 };
 
 await compress();
+
