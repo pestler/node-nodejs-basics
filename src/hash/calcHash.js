f17d1eb0d16e6ba316a0e743a7003e130630f774
Aleh Kuis <pestleralex@gmail.com>
2024-10-06 12:05:20 +0300

    feat: calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import fs from 'fs';
+const {
+    createHash
+} = await import('crypto');
+import { getPathUrl } from '../util/get-url-path.js';
+
+const filesDir = 'files';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.resolve(getPathUrl(import.meta.url),filesDir, 'fileToCalculateHashFor.txt')
+    const buffer = fs.readFileSync(filePath)
+    const hash = createHash('sha256').update(buffer).digest('hex')
+    console.log(`SHA256 hash: ${hash}`);
 };
 
 await calculateHash();
+
