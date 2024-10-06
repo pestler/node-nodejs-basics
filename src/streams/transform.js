2dc63b0b3529951342a25648da1a431f975f7a1c
Aleh Kuis <pestleralex@gmail.com>
2024-10-06 16:56:18 +0300

    feat: transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,26 @@
+import { Transform } from 'stream';
+import { pipeline } from 'stream';
+
+const reversText = (data) => data
+    .split('')
+    .reverse()
+    .join('')
+    .concat('\n');
+
+export const reverseTransform = new Transform({
+    transform(chunk, _encoding, callback) {
+        this.push(reversText(chunk.toString()));
+        callback();
+    }
+});
+
 const transform = async () => {
-    // Write your code here 
+
+    pipeline(process.stdin, reverseTransform, process.stdout,
+        (error) => error && process.stderr.write(error)
+    )
+    console.log('Enter text:');
 };
 
 await transform();
+
