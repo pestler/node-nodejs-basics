515d5e1e304c9a6290ec09a9752e8a197c173d59
Aleh Kuis <pestleralex@gmail.com>
2024-10-06 15:56:03 +0300

    feat: write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+import path from 'path'
+import { createWriteStream } from 'fs';
+import { stdin, stderr, exit } from 'process';
+import { getPathUrl } from '../util/get-url-path.js';
+
+const filesDir = 'files';
 const write = async () => {
-    // Write your code here 
+    const fileWrite = path.resolve(getPathUrl(import.meta.url), filesDir, 'fileToWrite.txt')
+    const fileToWrite = createWriteStream(fileWrite)
+
+    stdin.pipe(fileToWrite).on('error', (err) => stderr.write(err))
+    process.on('SIGINT', () => {
+        fileToWrite.end();
+        exit();
+    })    
 };
 
 await write();
+
+
