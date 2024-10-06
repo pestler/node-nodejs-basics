01d42658769b42970326146dc771e698673a3ed0
Aleh Kuis <pestleralex@gmail.com>
2024-10-06 14:29:50 +0300

    feat: read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import path from 'path'
+import { createReadStream } from 'fs';
+import { getPathUrl } from '../util/get-url-path.js';
+
+const filesDir = 'files';
+
 const read = async () => {
-    // Write your code here 
+const fileRead = path.resolve(getPathUrl(import.meta.url), filesDir, 'fileToRead.txt')
+const streamRead = createReadStream(fileRead)
+streamRead.pipe(process.stdout, { end: false });
 };
 
 await read();
+
+
