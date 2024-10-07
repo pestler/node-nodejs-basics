2343515a4c69e6f843bb4035d1513d0ea4debef2
Aleh Kuis <pestleralex@gmail.com>
2024-10-07 18:30:37 +0300

    feat: cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import path from 'path';
+import { fork } from 'child_process';
+
+import { getPathUrl } from '../util/get-url-path.js'
+const filesDir = 'files';
+
+export const spawnChildProcess = async (args) => {
+    
+    const sourcePath = path
+    .resolve(getPathUrl(import.meta.url), filesDir, 'script.js');
+
+    fork(sourcePath, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['Argument1', 'Argument2', 'Argument3']);
+
--- src/fs/list.js
+++ src/fs/list.js
@@ -3,8 +3,8 @@ import { existsSync, readdirSync } from 'fs';
 import { getPathUrl } from '../util/get-url-path.js'
 import { PropertyRequiredError } from '../util/validation-error.js'
 
-const source = 'files';
-const sourcePath = path.resolve(getPathUrl(import.meta.url), source);
+const filesDir = 'files';
+const sourcePath = path.resolve(getPathUrl(import.meta.url), filesDir);
 
 const list = async () => {
     try {
