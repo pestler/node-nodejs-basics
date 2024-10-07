672163ee55b4d2b890e9d67106c59578ebb770f4
Aleh Kuis <pestleralex@gmail.com>
2024-10-07 16:24:59 +0300

    feat: main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { cpus } from 'os';
+import path from 'path';
+import { Worker } from 'worker_threads';
+
+import { getPathUrl } from '../util/get-url-path.js'
+
 const performCalculations = async () => {
-    // Write your code here
+    const workerPath = path.resolve(getPathUrl(import.meta.url), 'worker.js');
+
+    let workerArr = [];
+    let number = 10
+    const arrWorkers = cpus().map((_el, i) => {
+        return new Promise((resolve, reject) => {
+            const myWorker = new Worker(workerPath);
+            workerArr
+                .push(myWorker);
+            myWorker
+                .postMessage(number + i);
+            myWorker
+                .on("message", (data) => resolve({ status: 'resolved', data }));
+            myWorker
+                .on("error", () => reject({ status: 'error', data: null }));
+        });
+    });
+
+    const res = await Promise.all(arrWorkers);
+    for (const worker of workerArr) {
+        worker.terminate();
+    }
+    console.log(res);
 };
 
+
 await performCalculations();
+
+
+
+
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,11 +1,12 @@
 import { workerData, parentPort } from 'worker_threads';
 
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-const sendResult = () => {
-    parentPort.postMessage(nthFibonacci(workerData))
+export const sendResult = () => {
+    parentPort.once('message', (message) => {
+        parentPort.postMessage(nthFibonacci(message));
+    });
 };
 
 sendResult();
-
 
